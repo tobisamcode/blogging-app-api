93d3055d05650c81c0bb1255a95d83ba6baf6c3c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-30 01:07:05 +0100

    implementing the user login and sign up model/schema

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const userRouter = express.Router();
+
+// login route
+userRouter.post("/login", () => {});
+
+// signup route
+userRouter.post("/signup", () => []);
