897b77f48f8872248d8458cacfb5ea5b964bf403
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-11-07 02:02:34 +0100

    implementing the user login and sign up model/schema

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,7 @@ const loginUser = async (req, res) => {
     // createToken
     const token = createToken(user._id, email);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ message: "Login successful", email, token });
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
@@ -33,7 +33,10 @@ const signupUser = async (req, res) => {
     // create token
     const token = createToken(user._id, email);
 
-    res.status(200).json({ email, first_name, last_name, token });
+    res.status(200).json({
+      message: "Signup was successful",
+      user: { email, first_name, last_name, token }
+    });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
